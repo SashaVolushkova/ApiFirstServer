e57e97f744b4431c2fd2f475b344e3cd8687372d
alexandra.volushkova <alexandra.volushkova@ax-team.com>
2023-08-07 14:26:19 +0300

    change port

--- src/main/java/com/volushkova/apifirst/apifirstserver/controller/PetApiImpl.java
+++ src/main/java/com/volushkova/apifirst/apifirstserver/controller/PetApiImpl.java
@@ -5,6 +5,7 @@ import com.volushkova.apifirst.generated.model.Pet;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -17,7 +18,18 @@ public class PetApiImpl implements PetsApi {
 
     @Override
     public ResponseEntity<List<Pet>> listPets(Integer limit) {
+        if(limit == null) {
+            limit = 10;
+        }
+        List<Pet> pets = new ArrayList<>();
         // your future implementation
-        return null;
+        for(long i = 0; i < limit; i++) {
+            Pet pet = new Pet();
+            pet.setTag("teg_" + i);
+            pet.setName("name_" + i);
+            pet.id(i);
+            pets.add(pet);
+        }
+        return ResponseEntity.ok(pets);
     }
 }
